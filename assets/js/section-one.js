f232378647dc0ad07a4e7bafd78119a376fb91b1
Danial Alborz <danialalborz3@gmail.com>
2024-10-21 00:55:06 +0330

    half of  section one

---
+++ assets/js/section-one.js
@@ -0,0 +1,16 @@
+let menu = document.querySelector('#menu-ul')
+let meet = document.querySelector('#meet')
+let black = document.querySelector('#black')
+
+console.log(menu);
+function opacityTrue() {
+    menu.style.opacity = '1';
+    // menu.style.t = '1';
+    // menu.classList.remove 
+    // menu.style.display = 'block';
+}
+
+setTimeout(() => {
+    opacityTrue()
+    // mobile
+}, 1000);
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,27 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'Manrope': ['Manrope']
+        'manrope': ['manrope'],
+        'manrope-bold': ['manrope-bold'],
+
       },
       boxShadow: {
         "sign-up-btn": '0 0 12px white',
+      },
+      animation: {
+        'meet': "meet-black 2s linear forwards",
+        'black': "meet-black 2s .5s linear forwards"
+      },
+      keyframes: {
+        'meet-black': {
+          '0%, 50%': {
+            transform: 'rotateX(90deg)'
+          },
+          "50%, 100%": {
+            transform: "rotateX(0deg)"
+          }
+
+        }
       }
     },
   },
