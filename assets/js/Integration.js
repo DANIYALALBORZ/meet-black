b282a976b622861b4da75c8b3661e9ece94ebf56
Tara niaki <taraniaki@gmail.com>
2024-10-22 15:27:08 -0400

    js fix1

--- assets/js/Integration.js
+++ assets/js/Integration.js
@@ -1,4 +1,19 @@
 
 const myIcon = document.querySelectorAll('#icons>img');
+const wIcons = myIcon[0]?.clientWidth
+const hIcons = myIcon[0]?.clientHeight
 
+
+
+myIcon.forEach(val => {
+    val.addEventListener('mousemove', (e) => {
+
+        console(val)
+        // let x = ((e.offsetX) / 10) - (wIcons / 20)
+        // let y = ((e.offsetY) / 10) - (hIcons / 20)
+        // console.log(y);
+
+        // e.target.style.transform = 'perspective(800px) rotateY(' + x + 'deg) rotateX(' + y + 'deg)'
+    })
+})
 
